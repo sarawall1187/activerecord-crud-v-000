2cb848b475d5a72edffa5d720cfd33d9ef601155
Sara Walker <sarawall1187@gmail.com>
2019-02-21 21:10:24 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -69,11 +69,9 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  Movie.find_by(title: "Awesome Flick")
-  self.update(title: "Even Awesomer Flick")
-  self.save
-  __
-  __
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.update(title: "Even Awesomer Flick")
+  movie.save
 end
 
 def can_update_using_update_method
