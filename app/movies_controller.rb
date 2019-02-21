435afd957af9c11e41871695e946a6f53538f473
Sara Walker <sarawall1187@gmail.com>
2019-02-21 19:23:25 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -58,10 +58,10 @@ def can_find_by_multiple_attributes(title = "Title", release_date = 2000, direct
   Movie.find_by(title: title, release_date: release_date, director: director)
 end
 
-def can_find_using_where_clause_and_be_sorted(release_date = 2002)
+def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.where(release_date >= 2002)
+  Movie.where(release_date: = 2002).ordered(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
